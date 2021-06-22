f28a77f06203ebefd2d75e0111fb4d8f825d33d0
Michael Eredia <erediamichaelj@gmail.com>
2021-06-22 21:13:08 +0200

    Witty retort

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -9,6 +9,7 @@ namespace communication_log
 			Console.WriteLine("Hello pilot");
             Console.WriteLine("Sup");
 			Console.WriteLine("You are in restricted air space!");
+			string YourMom = ("restricted air space!");
 		}
 	}
 }
