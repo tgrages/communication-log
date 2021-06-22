def01411ec49008acac7bdca630fe04161bf2940
Tom Grages <tgrages_12@hotmail.com>
2021-06-22 15:43:19 -0400

    added text with degrees

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -13,6 +13,8 @@ namespace communication_log
 			Console.WriteLine("Very clever.");
             Console.WriteLine("Okay i'll stay out of " + YourMom );
 			int divertNewCoordinates = 360;
+			Console.WriteLine($"Please divert your plane to {Int32.Parse(divertNewCoordinates)} degrees");
+
 		}
 	}
 }
